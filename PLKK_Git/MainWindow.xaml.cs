9f15342826ace7078fd70315b8f5be0c57291ffd
PLabuz <labuzpat05@gmail.com>
2024-01-22 09:35:34 +0100

    Dodanie kodu cs dla dodawania zadań oraz zczytywania zadań z pliku json

--- PLKK_Git/MainWindow.xaml.cs
+++ PLKK_Git/MainWindow.xaml.cs
@@ -8,6 +8,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace PLKK_Git
 {
@@ -16,9 +18,53 @@ namespace PLKK_Git
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string path = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ToDo.json");
+        public List<Tasks> TasksParam = new List<Tasks>();
         public MainWindow()
         {
             InitializeComponent();
+
+            //ReadTasks();
+        }
+        public void ReadTasks()
+        {
+            string serializedTasks = File.ReadAllText(path);
+            List<Tasks> tasks = JsonConvert.DeserializeObject<List<Tasks>>(serializedTasks);
+
+            TasksParam = tasks;
+
+            ToDoList.ItemsSource = TasksParam;
+        }
+        public void AddTask()
+        {
+            Tasks task = new Tasks();
+            task.Title = Title.Text;
+            task.Description = Title.Text;
+            task.Deadline = Date.SelectedDate;
+            task.Priority = Priority.Text;
+
+            TasksParam.Add(task);
+
+            if(File.Exists(path))
+            {
+                string serializedTasks = JsonConvert.SerializeObject(TasksParam);
+
+                File.WriteAllText(path, serializedTasks);
+
+                ReadTasks();
+            }
+            else
+            {
+                string serializedTasks = JsonConvert.SerializeObject(new List<Tasks>());
+
+                File.WriteAllText(path, serializedTasks);
+
+                ReadTasks();
+            }
+        }
+        public void DeleteTask()
+        {
+
         }
     }
 }
---
+++ PLKK_Git/Tasks.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLKK_Git
+{
+    public class Tasks
+    {
+        public string Title {  get; set; }
+        public string Description { get; set; }
+        public DateTime Deadline { get; set; }
+        public string Priority { get; set; }
+    }
+}
